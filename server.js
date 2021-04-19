78499fc4d165688ec60b82fa65231437bb8c3c85
AlexD2019 <alexander0derevianko@gmail.com>
2021-04-19 13:51:55 +0300

    Getting gifs from server, sorting them by rating, saving first 10 to files

--- server.js
+++ server.js
@@ -11,4 +11,43 @@ function listening() {
 
 app.use(express.static('website'))
 
+let request = require('request');
+let fs = require('fs');
+let https = require('https');
+const sendGetRequest = function () {
+	const apiKey = 'MZeFSXlrHBVGA5U53AdxCchasHaTkIk3';
+	const word = 'cheeseburgers';
+	const uri = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${word}`;
+	
+	request(uri, (error, response, body) => {
+		if (!error && response.statusCode === 200) {
+			let gifs = [];
+			const objects = JSON.parse(body);
+			
+			for (const object of objects.data) {
+				const gif = {
+					id: object.id,
+					url: object.images.original.url,
+					rating: object.rating,
+				}
+				
+				gifs.push(gif);
+			}
+			
+			gifs.sort((a, b) =>
+				a.rating < b.rating ? -1 : a.rating > b.rating ? 1 : 0);
+			
+			const firstTen = gifs.slice(0, 10);
+			for (const gif of firstTen) {
+				const file = fs.createWriteStream(`gifs/${gif.id}.gif`);
+				const request = https.get(gif.url, function (response) {
+					response.pipe(file);
+				})
+			}
+			
+			console.log('Done');
+		}
+	});
+}
+sendGetRequest();
 
